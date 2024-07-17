98294e14b6685d7d417360016178775ef7e7bda1
Banny101 <banny@nexusflowinnovations.com>
2024-07-17 22:51:07 +1000

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -235,6 +235,7 @@ export const MultiSelectExtension = {
           border: 1px solid #ced4da;
           cursor: pointer;
           transition: background-color 0.3s, border-color 0.3s;
+          color: black;
         }
         .multi-select-options input[type="checkbox"] {
           margin-right: 10px;
