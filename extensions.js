37613bf3c68954660917799fabeecf2d061b6255
Banny101 <banny@nexusflowinnovations.com>
2024-10-11 23:38:42 +1100

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -736,3 +736,45 @@ export const DelayEffectExtension = {
     window.voiceflow.chat.interact({ type: "complete" });
   }
 };
+
+export const CalendlyExtension = {
+  name: "Calendly",
+  type: "effect",
+  match: ({ trace }) => {
+    return (
+      trace.type === "ext_calendly" || trace.payload.name === "ext_calendly"
+    );
+  },
+  effect: ({ trace }) => {
+    const { url } = trace.payload;
+    if (url) {
+      Calendly.initPopupWidget({ url });
+    }
+  },
+};
+
+export const VideoExtension = {
+  name: "Video",
+  type: "response",
+  match: ({ trace }) =>
+    trace.type === "ext_video" || trace.payload.name === "ext_video",
+  render: ({ trace, element }) => {
+    const videoElement = document.createElement("video");
+    const { videoURL, autoplay, controls } = trace.payload;
+
+    videoElement.width = 240;
+    videoElement.src = videoURL;
+
+    if (autoplay) {
+      videoElement.setAttribute("autoplay", "");
+    }
+    if (controls) {
+      videoElement.setAttribute("controls", "");
+    }
+
+    videoElement.addEventListener("ended", function () {
+      window.voiceflow.chat.interact({ type: "complete" });
+    });
+    element.appendChild(videoElement);
+  },
+};
