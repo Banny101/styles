3be850e2f0e9554ecd01ec0353c63097b4250e05
Banny101 <banny@nexusflowinnovations.com>
2024-10-17 21:43:56 +1100

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -693,3 +693,77 @@ export const DelayEffectExtension = {
     window.voiceflow.chat.interact({ type: "complete" });
   }
 };
+
+export const FileUploadExtension = {
+  name: 'FileUpload',
+  type: 'response',
+  match: ({ trace }) =>
+    trace.type === 'ext_fileUpload' || trace.payload.name === 'ext_fileUpload',
+  render: ({ trace, element }) => {
+    const fileUploadContainer = document.createElement('div')
+    fileUploadContainer.innerHTML = `
+      <style>
+        .my-file-upload {
+          border: 2px dashed rgba(46, 110, 225, 0.3);
+          padding: 20px;
+          text-align: center;
+          cursor: pointer;
+        }
+      </style>
+      <div class='my-file-upload'>Drag and drop a file here or click to upload</div>
+      <input type='file' style='display: none;'>
+    `
+
+    const fileInput = fileUploadContainer.querySelector('input[type=file]')
+    const fileUploadBox = fileUploadContainer.querySelector('.my-file-upload')
+
+    fileUploadBox.addEventListener('click', function () {
+      fileInput.click()
+    })
+
+    fileInput.addEventListener('change', function () {
+      const file = fileInput.files[0]
+      console.log('File selected:', file)
+
+      fileUploadContainer.innerHTML = `<img src="https://s3.amazonaws.com/com.voiceflow.studio/share/upload/upload.gif" alt="Upload" width="50" height="50">`
+
+      var data = new FormData() 
+      data.append('UPLOADCARE_PUB_KEY', trace.payload.apiKey)
+      data.append('UPLOADCARE_STORE', 'auto')
+      data.append('file', file, file.name)
+
+      fetch('https://upload.uploadcare.com/base/', {
+        method: 'POST',
+        body: data,
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('Upload failed: ' + response.statusText)
+          }
+        })
+        .then((result) => {
+          fileUploadContainer.innerHTML =
+            '<img src="https://s3.amazonaws.com/com.voiceflow.studio/share/check/check.gif" alt="Done" width="50" height="50">'
+          console.log('File uploaded:', result.file)
+
+          const fileUUID = result.file
+          const fileURL = 'https://ucarecdn.com/' + fileUUID + '/' + encodeURIComponent(file.name)
+
+          window.voiceflow.chat.interact({
+            type: 'complete',
+            payload: {
+              file: fileURL,
+            },
+          })
+        })
+        .catch((error) => {
+          console.error(error)
+          fileUploadContainer.innerHTML = '<div>Error during upload</div>'
+        })
+    })
+
+    element.appendChild(fileUploadContainer)
+  },
+}
